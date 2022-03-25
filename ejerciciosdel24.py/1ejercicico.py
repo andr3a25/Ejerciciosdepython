c852be453e3b25ac1f182b6eeaf798178cc45a7c
andr3a25 <101885108+andr3a25@users.noreply.github.com>
2022-03-25 10:59:46 -0500

    clase 24

---
+++ ejerciciosdel24.py/1ejercicico.py
@@ -0,0 +1,4 @@
+name = input ("introduzca su nombre,  por favor: ")
+surname = input ("introduzca su apellido, por favor: ")
+
+print(f"{surname} , {name}")
---
+++ ejerciciosdel24.py/4ejercicio.py
@@ -0,0 +1,6 @@
+valor = 5
+
+valor = str (valor)
+solucion = int(valor) + int (valor * 2) + int (valor * 3)
+
+print(solucion)
