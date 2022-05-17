177dafd94e9061fae6a1f07908511a9fc1641160
andr3a25 <101885108+andr3a25@users.noreply.github.com>
2022-05-16 21:49:50 -0500

    actualizar listas

---
+++ diccionario.py/dic.py
@@ -0,0 +1,42 @@
+month=dict(
+    enero=1,
+    febrero=2,
+    marzo=3,
+    abril=4,
+    mayo=5,
+    junio=6,
+    julio=7,
+    agosto=8,
+    septiembre=9,
+    octubre=10,
+    noviembre=11,
+    diciembre=12,
+)
+total=0
+
+
+for clave, valor in month.items():
+
+    max_values=max(month, key=month.get)
+    
+print('El mes con mayor produccion es: ',max_values,' y su valor es  ', month[max_values])
+
+for clave, valor in month.items():
+
+    min_values=min(month, key=month.get)
+    
+print('El mes con menor produccion es: ',min_values, 'y su valor es:', month[min_values] )
+
+for i in month.values():
+    total += i
+promedio= total/12
+
+print('El valor promedio es: ',promedio)
+
+for key in month.keys():
+    if month[key]>= promedio:
+        print('Los meses por encima del promedio son: ', key)
+
+for key in month.keys():
+    if month[key]<= promedio:
+        print('Los meses por debajo del promedio son: ', key)
